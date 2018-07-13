0c81b920ee574ca8ec44d7c8c4145d97d7707882
mxiong11 <mxiong11@bloomberg.net>
2018-07-13 08:41:49 -0700

    add demo instructions

--- src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
+++ src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
@@ -15,17 +15,23 @@
  */
 package io.confluent.examples.streams;
 
+import io.confluent.examples.streams.avro.WordCount;
+import io.confluent.examples.streams.avro.Words;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.KeyValue;
 
-import java.util.Arrays;
-import java.util.Properties;
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
@@ -49,10 +55,10 @@ import java.util.regex.Pattern;
  * 2) Create the input and output topics used by this example.
  * <pre>
  * {@code
- * $ bin/kafka-topics --create --topic streams-plaintext-input \
- *                    --zookeeper localhost:2181 --partitions 1 --replication-factor 1
- * $ bin/kafka-topics --create --topic streams-wordcount-output \
- *                    --zookeeper localhost:2181 --partitions 1 --replication-factor 1
+ * $ bin/kafka-topics --create --topic kakfa-stream-demo \
+ *                    --zookeeper 100.83.16.148:2181 --partitions 1 --replication-factor 1
+ * $ bin/kafka-topics --create --topic kakfa-stream-demo-output \
+ *                    --zookeeper 100.83.16.148:2181 --partitions 1 --replication-factor 1
  * }</pre>
  * Note: The above commands are for the Confluent Platform. For Apache Kafka it should be {@code bin/kafka-topics.sh ...}.
  * <p>
@@ -62,7 +68,11 @@ import java.util.regex.Pattern;
  * Once packaged you can then run:
  * <pre>
  * {@code
- * $ java -cp target/kafka-streams-examples-4.0.0-SNAPSHOT-standalone.jar io.confluent.examples.streams.WordCountLambdaExample
+ * $ export KAFKA_BOOSTRAP_SERVER=PLAINTEXT://dob2-bach-r2n09.bloomberg.com:6667
+ * $ export SCHEMA_REGISTRY_SERVER=https://schema-reg-dob2.ing.spaas-nj-dev01.bce.bloomberg.com
+ * $ export KAFKA_INPUT_TOPIC=kakfa-stream-demo
+ * $ export KAFKA_OUTPUT_TOPIC=kakfa-stream-demo-output
+ * $ java -cp target/kafka-streams-examples-4.1.1-standalone.jar io.confluent.examples.streams.WordCountLambdaExample
  * }
  * </pre>
  * 4) Write some input data to the source topic "streams-plaintext-input" (e.g. via {@code kafka-console-producer}).
@@ -70,22 +80,24 @@ import java.util.regex.Pattern;
  * results to the output topic "streams-wordcount-output".
  * <pre>
  * {@code
- * # Start the console producer. You can then enter input data by writing some line of text, followed by ENTER:
- * #
- * #   hello kafka streams<ENTER>
- * #   all streams lead to kafka<ENTER>
- * #   join kafka summit<ENTER>
+ * # Use reset proxy to write to input topic. You can then enter input data by writing some line of text, followed by ENTER:
+ * $curl  https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/topics/kakfa-stream-demo
+ * $curl -X POST -H "Content-Type: application/vnd.kafka.avro.v1+json" --data '{"value_schema": "{\"type\": \"record\", \"name\": \"Words\", \"fields\": [{\"name\": \"words\", \"type\": \"string\"}]}", "records": [{"value": {"words": "hello kafka streams"}}]}' "https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/topics/kakfa-stream-demo"
+ * $curl -X POST -H "Content-Type: application/vnd.kafka.avro.v1+json" --data '{"value_schema": "{\"type\": \"record\", \"name\": \"Words\", \"fields\": [{\"name\": \"words\", \"type\": \"string\"}]}", "records": [{"value": {"words": "all streams lead to kafka"}}]}' "https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/topics/kakfa-stream-demo"
+ * $curl -X POST -H "Content-Type: application/vnd.kafka.avro.v1+json" --data '{"value_schema": "{\"type\": \"record\", \"name\": \"Words\", \"fields\": [{\"name\": \"words\", \"type\": \"string\"}]}", "records": [{"value": {"words": "join kafka summit"}}]}' "https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/topics/kakfa-stream-demo"
+ * #   hello kafka streams
+ * #   all streams lead to kafka
+ * #   join kafka summit
  * #
  * # Every line you enter will become the value of a single Kafka message.
- * $ bin/kafka-console-producer --broker-list localhost:9092 --topic streams-plaintext-input
  * }</pre>
  * 5) Inspect the resulting data in the output topic, e.g. via {@code kafka-console-consumer}.
  * <pre>
  * {@code
- * $ bin/kafka-console-consumer --topic streams-wordcount-output --from-beginning \
- *                              --new-consumer --bootstrap-server localhost:9092 \
- *                              --property print.key=true \
- *                              --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+ * $ curl -X POST -H "Content-Type: application/vnd.kafka.v1+json" --data '{"name": "my_consumer_instance", "format": "avro", "auto.offset.reset": "smallest"}' https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/consumers/my_avro_consumer
+ * $ curl -X GET -H "Accept: application/vnd.kafka.avro.v1+json" https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/consumers/my_avro_consumer/instances/my_consumer_instance/topics/kakfa-stream-demo
+ * $ curl -X POST -H "Content-Type: application/vnd.kafka.v1+json" --data '{"name": "my_consumer_instance-output", "format": "avro", "auto.offset.reset": "smallest"}' https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/consumers/my_avro_consumer
+ * $ curl -X GET -H "Accept: application/vnd.kafka.avro.v1+json" https://rest-proxy-dob2.ing.spaas-nj-dev01.bce.bloomberg.com/consumers/my_avro_consumer/instances/my_consumer_instance-output/topics/kakfa-stream-demo-output
  * }</pre>
  * You should see output data similar to below. Please note that the exact output
  * sequence will depend on how fast you type the above sentences. If you type them
@@ -111,8 +123,14 @@ import java.util.regex.Pattern;
  */
 public class WordCountLambdaExample {
 
+  private static final String kafka_bootstrap_server = "KAFKA_BOOTSTRAP_SERVER";
+  private static final String schema_registry_server = "SCHEMA_REGISTRY_SERVER";
+  private static final String kafka_input_topic = "KAFKA_INPUT_TOPIC";
+  private static final String kafka_output_topic = "KAFKA_OUTPUT_TOPIC";
+
   public static void main(final String[] args) throws Exception {
-    final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9092";
+    final String bootstrapServers = Optional.ofNullable(System.getenv(kafka_bootstrap_server)).orElse("localhost:9092");
+    final String schemaRegistryUrl = Optional.ofNullable(System.getenv(schema_registry_server)).orElse("localhost:8081");
     final Properties streamsConfiguration = new Properties();
     // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
     // against which the application is run.
@@ -120,9 +138,10 @@ public class WordCountLambdaExample {
     streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "wordcount-lambda-example-client");
     // Where to find Kafka broker(s).
     streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    streamsConfiguration.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
     // Specify default (de)serializers for record keys and for record values.
     streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass().getName());
     // Records should be flushed every 10 seconds. This is less than the default
     // in order to keep this example interactive.
     streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
@@ -133,6 +152,8 @@ public class WordCountLambdaExample {
     // specified above.
     final Serde<String> stringSerde = Serdes.String();
     final Serde<Long> longSerde = Serdes.Long();
+    final SpecificAvroSerde<Words> wordsSerde = createSerde(schemaRegistryUrl);
+    final SpecificAvroSerde<WordCount> wordCountSerde = createSerde(schemaRegistryUrl);
 
     // In the subsequent lines we define the processing topology of the Streams application.
     final StreamsBuilder builder = new StreamsBuilder();
@@ -145,7 +166,8 @@ public class WordCountLambdaExample {
     // the default serdes specified in the Streams configuration above, because these defaults
     // match what's in the actual topic.  However we explicitly set the deserializers in the
     // call to `stream()` below in order to show how that's done, too.
-    final KStream<String, String> textLines = builder.stream("streams-plaintext-input");
+    final KStream<String, Words> textLines = builder.stream(Optional.ofNullable(System.getenv(kafka_input_topic))
+            .orElse("streams-plaintext-input"), Consumed.with(stringSerde, wordsSerde));
 
     final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
 
@@ -153,10 +175,10 @@ public class WordCountLambdaExample {
       // Split each text line, by whitespace, into words.  The text lines are the record
       // values, i.e. we can ignore whatever data is in the record keys and thus invoke
       // `flatMapValues()` instead of the more generic `flatMap()`.
-      .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+      .flatMapValues(value -> Arrays.asList(pattern.split(value.getWords().toLowerCase())))
       // Count the occurrences of each word (record key).
       //
-      // This will change the stream type from `KStream<String, String>` to `KTable<String, Long>`
+      // This will change the stream type from `KStream<String, Words>` to `KTable<String, Long>`
       // (word -> count).  In the `count` operation we must provide a name for the resulting KTable,
       // which will be used to name e.g. its associated state store and changelog topic.
       //
@@ -165,7 +187,9 @@ public class WordCountLambdaExample {
       .count();
 
     // Write the `KTable<String, Long>` to the output topic.
-    wordCounts.toStream().to("streams-wordcount-output", Produced.with(stringSerde, longSerde));
+    wordCounts.toStream().map((key, value) -> new KeyValue<>(key,  new WordCount(key, value)))
+            .to(Optional.ofNullable(System.getenv(kafka_output_topic))
+            .orElse("streams-wordcount-output"), Produced.with(stringSerde, wordCountSerde));
 
     // Now that we have finished the definition of the processing topology we can actually run
     // it via `start()`.  The Streams application as a whole can be launched just like any
@@ -188,4 +212,12 @@ public class WordCountLambdaExample {
     Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
   }
 
+  private static <VT extends SpecificRecord> SpecificAvroSerde<VT> createSerde(final String schemaRegistryUrl) {
+
+    final SpecificAvroSerde<VT> serde = new SpecificAvroSerde<>();
+    final Map<String, String> serdeConfig = Collections.singletonMap(
+            AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+    serde.configure(serdeConfig, false);
+    return serde;
+  }
 }
